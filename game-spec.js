f687f54a2c61890e09196559fa1551d96dcc7aa3
Jason Duffy <jasonsduffy@gmail.com>
2016-07-28 23:13:13 -0400

    enough neighbors to survive

--- game-spec.js
+++ game-spec.js
@@ -5,20 +5,69 @@ describe('game', function () {
   describe('Any live cell with fewer than two live neighbors dies, as if caused by under-population.', function () {
 
     it('single cell does not survive', function () {
-      expect(game.iterate([[1]])).toEqual([[0]]);
+      var before = [[0, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 0]];
+
+      var after = [[0, 0, 0],
+                   [0, 0, 0],
+                   [0, 0, 0]];
+
+      expect(game.iterate(before)).toEqual(after);
     });
 
     it('cells with only one neighbor do not survive', function () {
-      expect(game.iterate([[1, 1]])).toEqual([[0, 0]]);
+      var before = [[0, 0, 0, 0],
+                    [0, 1, 1, 0],
+                    [0, 0, 0, 0]];
+
+      var after = [[0, 0, 0, 0],
+                   [0, 0, 0, 0],
+                   [0, 0, 0, 0]];
+
+      expect(game.iterate(before)).toEqual(after);
     });
 
     it('lonely cells in a grid', function () {
-      expect(game.iterate([[0, 1], [1, 0]])).toEqual([[0, 0],[0, 0]]);
+      var before = [[0, 0, 0, 1],
+                    [0, 0, 0, 0],
+                    [0, 1, 0, 0]];
+
+      var after = [[0, 0, 0, 0],
+                   [0, 0, 0, 0],
+                   [0, 0, 0, 0]];
+
+      expect(game.iterate(before)).toEqual(after);
     });
   });
 
   describe('Any live cell with two or three live neighbors lives on to the next generation.', function () {
 
+    it('two neighbors', function () {
+      var before = [[0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0]];
+
+      var after = [[0, 0, 0, 0, 0],
+                   [0, 0, 1, 0, 0],
+                   [0, 0, 0, 0, 0]];
+
+      expect(game.iterate(before)).toEqual(after);
+    });
+
+    it('three neighbors', function () {
+      var before = [[0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0]];
+
+      var after = [[0, 0, 0, 0, 0],
+                   [0, 0, 1, 0, 0],
+                   [0, 1, 0, 1, 0],
+                   [0, 0, 0, 0, 0]];
+
+      expect(game.iterate(before)).toEqual(after);
+    });
   });
 
   describe('Any live cell with more than three live neighbors dies, as if by over-population.', function () {
--- game.js
+++ game.js
@@ -1,9 +1,25 @@
 var _ = require('lodash');
 
 exports.iterate = function (grid) {
-  return _.map(grid, function (row) {
-    return _.map(row, function () {
-      return 0;
+  return _.map(grid, function (row, rowNum) {
+    return _.map(row, function (column, columnNum) {
+      var neighbors = getNeighbors(grid, rowNum, columnNum);
+      return neighbors > 1 ? 1 : 0;
     });
   });
 };
+
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function getNeighbors(grid, rowNum, columnNum) {
+  var rows = grid.length;
+  var columns = grid[0].length;
+  var above = grid[mod(rowNum - 1, rows)][columnNum];
+  var below = grid[mod(rowNum + 1, rows)][columnNum];
+  var left = grid[rowNum][mod(columnNum - 1, columns)];
+  var right = grid[rowNum][mod(columnNum + 1, columns)];
+  
+  return _.sum([above, below, left, right]);
+}
